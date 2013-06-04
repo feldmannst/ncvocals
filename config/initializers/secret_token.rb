5a524c744777c501e60aea18ac1d7f529a7615e3
Scott <feldmannst@gmail.com>
2013-06-04 17:50:38 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ncvocals::Application.config.secret_token = '978ccb3dbd67bdcff31b6e75a0199dfa105f79145514755001014ff41d3e5146b71cb293aff6916eebd5c4bad5bf7a9a5361a6a5427e9f27979bf8e71ed28061'
